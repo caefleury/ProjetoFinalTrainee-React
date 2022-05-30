dc789ec3f414d89c1c5cdd22c7502bfed54eaa8c
caefleury <caefleury15@gmail.com>
2022-05-30 20:18:41 -0300

    :memo: limpando e configurando o App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
+import './global.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Struct
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
