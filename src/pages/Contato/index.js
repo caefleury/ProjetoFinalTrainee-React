856bec73c6e73140a68f03a11de5b7964cc3be96
Pedro Ximenes <pedroximenes76@gmail.com>
2022-06-20 23:09:26 -0300

    🧱 Alterando estrutura da página Contato

--- src/pages/Contato/index.js
+++ src/pages/Contato/index.js
@@ -1,4 +1,5 @@
 import Navbar from '../../components/Navbar';
+import Contact from '../../components/Contact';
 import Home from '../../components/Home';
 import Footer from '../../components/Footer';
 
@@ -6,7 +7,7 @@ const Contato = () =>{
     return(
         <>
             <Navbar/>
-            <Home/>
+            <Contact/>
             <Footer/>
         </>
     )
